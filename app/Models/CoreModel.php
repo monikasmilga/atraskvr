b866b5547a2fc54faba07b1d7674a105df03c0c9
monikasmilga <tuuli705@gmail.com>
2017-05-23 14:16:02 +0300

    Revert "Created models to all DB tables"
    This reverts commit 9a52ef273f7855985426f507270d45277bba7c35.

--- app/Models/CoreModel.php
+++ app/Models/CoreModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Http\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class CoreModel extends Model
 {
