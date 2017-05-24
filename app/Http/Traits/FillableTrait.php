94aaad84a3868b060b359f79ebf3e75286244ffa
KarolisRazmus <karolisrazmus@gmail.com>
2017-05-24 10:44:12 +0300

    traits commit

---
+++ app/Http/Traits/FillableTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karolis
+ * Date: 5/24/2017
+ * Time: 10:40 AM
+ */
+
+namespace App\Http\Traits;
+
+
+trait FillableTrait
+{
+
+}
---
+++ app/Http/Traits/TableNameTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karolis
+ * Date: 5/24/2017
+ * Time: 10:41 AM
+ */
+
+namespace App\Http\Traits;
+
+
+trait TableNameTrait
+{
+
+}
