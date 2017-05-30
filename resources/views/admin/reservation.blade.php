47cf3c59c24c57c58152d4b79a31d42a4cce82ef
aivarasvain <aivarasvainius11@gmail.com>
2017-05-30 15:21:01 +0300

    Reservations

--- resources/views/admin/reservation.blade.php
+++ resources/views/admin/reservation.blade.php
@@ -4,46 +4,87 @@
 @section('content')
 
 
+    @foreach($days as $day)
+
+        <a href="{{$day}}">{{$day}}</a><br>
+
+    @endforeach
+
+    <br><br>
 
 
 
-        <form method="POST" action="{{route('app.reservations.store')}}">
 
-            @foreach($days as $day)
+    <form method="POST" action="{{route('app.reservations.store')}}">
 
-                {{--@if($today == $day)--}}
+        @foreach($days as $day)
+            <div class="date-checkbox-group">
 
-            {{$day}}
+                @if($day == $date_from_url)
+
+                    <p>{{$day}}</p>
 
                     @foreach($experiences as $experience)
+                        <div class="experience-checkbox-group">
+                            {{$experience['translations'][0]['title']}}
 
-                        {{$experience['translations'][0]['title']}}
+                            @foreach($times as $key => $value)
 
-                        @foreach($times as $key => $value)
+                                @if($key % 6 == 0)
 
-                            @if($key % 6 == 0)
+                                    <br>
 
-                                <br>
-
-                            @endif
-
-                            <input type="checkbox" name="{{$experience['id'] . '[]'}}" value="{{$day . ' ' . $value}}">{{$value}}
-
-                        @endforeach
+                                @endif
 
 
-                        <br>
+                                    @foreach($reservations as $reservation)
+
+                                        @foreach($reservation['time'] as $time)
+
+                                            @if($time == $day . ' ' . $value && $experience['id'] == $reservation['pages_id'])
+
+                                                <input style="outline: 1px solid red" type="checkbox" name="{{$experience['id'] . '[]'}}" value="{{$day . ' ' . $value}}" disabled>{{$value}}
+
+
+                                            @else(end($reservations) == $reservation)
+
+                                                @if($time !== $day . ' ' . $value)
+                                                    <input type="checkbox" name="{{$experience['id'] . '[]'}}" value="{{$day . ' ' . $value}}">{{$value}}
+                                                @endif
+
+                                            @endif
+
+
+                                        @endforeach
+
+                                    @endforeach
+
+
+
+                            @endforeach
+
+
+                            <br>
+                        </div>
                     @endforeach
 
-                {{--@endif--}}
+
+                @elseif($date_from_url == null)
+
+                    <h1>Nera datos</h1>
+
+
+                @endif
 
 
 
-                    <br>
-            @endforeach
-                {{csrf_field()}}
-                <input class="btn btn-sm btn-primary" type="submit">
-        </form>
+
+            </div>
+
+        @endforeach
+            {{csrf_field()}}
+            <input class="btn btn-sm btn-primary" type="submit">
+    </form>
 
 
 @endsection
