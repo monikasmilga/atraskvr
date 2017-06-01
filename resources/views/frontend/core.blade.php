cdaef903f73723166afae552069ca6350805a0d0
monikasmilga <tuuli705@gmail.com>
2017-06-01 15:26:51 +0300

    Front end core blade

---
+++ resources/views/frontend/core.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    @include('frontend.css')
+</head>
+<body>
+
+@include('frontend.menu')
+
+@yield('content')
+
+</body>
+@include('frontend.js')
+</html>
+
+
+
+
